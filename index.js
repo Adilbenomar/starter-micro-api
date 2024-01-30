b243a0f591e1bd17aefcb4e330368fca7a87f493
Adilbenomar <158207434+Adilbenomar@users.noreply.github.com>
2024-01-30 19:41:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json()); 
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+///////////////////////////////////////////////////////////////////////////////
+function writedata(data){
+await s3.putObject({
+            Body: JSON.stringify(data),
+            Bucket: "cyclic-muddy-foal-fatigues-eu-north-1",
+            Key: "data/data.json",
+        }).promise()
+}
+/////////////////////////////////////////////////////////////////////////////////
+ function readdata(){
+     let data = await s3.getObject({
+            Bucket: "cyclic-muddy-foal-fatigues-eu-north-1",
+            Key: "data/data.json",
+        }).promise()
 
-
+     return JSON.parse(data);
+ }
+/////////////////////////////////////////////////////////////////////////////////
 app.get('/', function (req, res) {
     let msg="Welcome  to API  cyclic server"
   res.send(msg)
